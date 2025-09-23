e634b8bd1de0abdfd72b3f6444dff59dfe104e5f
weriwq57-byte <weriwq57@gmail.com>
2025-09-23 09:22:36 +0300

    Add files via upload

---
+++ semester_1/lab3_pointers_and_arrays/lab2.4tr.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+
+
+int main() {
+    int n;
+
+    std::cout << "Введите натуральное число: ";
+    std::cin >> n;
+
+    
+        if (!(std::cin) || n < 1) {
+         std::cout << "Ошибка. Нужно ввести натуральное число!\n";
+        return 1;
+        }
+     
+   
+    std::cout << "\nСовершенные числа от 1 до " << n << ":\n";
+
+    for (int num = 1; num <= n; num++) {
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == num) {
+            std::cout << num <<  ";\n";
+        }
+    }
+
+    return 0;
+}
